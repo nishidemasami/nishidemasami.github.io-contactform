38c14a028e5239cf2b5c82965ed2c75acdbf36eb
NISHIDE, Masami <nishidemasami@outlook.com>
2026-05-11 00:29:24 +0000

    Add IAM grant roles for select-function-lambda in develop and main environments

--- infrastructure/liquibase_migrate/changes/015_aws_iam_grant_select-function-lambda-role-develop.sql
+++ infrastructure/liquibase_migrate/changes/015_aws_iam_grant_select-function-lambda-role-develop.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset 013_aws_iam_grant_select-function-lambda-role-develop:013
+--changeset 015_aws_iam_grant_select-function-lambda-role-develop:015
 
 -- Grant selectview role to select-function-lambda-role-develop
 AWS IAM GRANT selectview TO 'arn:aws:iam::672530906129:role/select-function-lambda-role-develop';
