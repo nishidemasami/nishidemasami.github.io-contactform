53787e7179c2b3ce521ea8a2b38851eeb12f160c
NISHIDE, Masami <nishidemasammi@gmail.com>
2026-05-12 17:24:20 +0900

    fix: specify login mechanisms for Authenticator component

--- testpage/app/page.tsx
+++ testpage/app/page.tsx
@@ -35,7 +35,7 @@ function AuthenticatedRedirect() {
 export default function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100">
-      <Authenticator>
+      <Authenticator loginMechanisms={['email']}>
         {({ user }) => (user ? <AuthenticatedRedirect /> : <></>)}
       </Authenticator>
     </div>
