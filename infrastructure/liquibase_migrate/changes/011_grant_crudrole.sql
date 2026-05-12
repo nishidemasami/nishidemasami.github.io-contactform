6d16ba3d681c926d156c5a3f3979a85faf1bd834
NISHIDE, Masami <nishidemasammi@gmail.com>
2026-05-12 12:54:31 +0900

    fix: update SQL grants to specify schema for role permissions

--- infrastructure/liquibase_migrate/changes/011_grant_crudrole.sql
+++ infrastructure/liquibase_migrate/changes/011_grant_crudrole.sql
@@ -3,4 +3,4 @@
 --changeset 011_grant_crudrole:011
 
 -- Grant crudrole role
-GRANT SELECT, INSERT, UPDATE, DELETE ON inquiries IN SCHEMA public TO crudrole;
+GRANT SELECT, INSERT, UPDATE, DELETE ON public.inquiries TO crudrole;
