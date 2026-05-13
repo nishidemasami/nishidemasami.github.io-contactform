69f5957b2c22ea137f91ed17a15516283ecd831a
NISHIDE, Masami <nishidemasammi@gmail.com>
2026-05-13 14:00:47 +0900

    Remove interactive terminal flag from Docker run command in SVG generation script

--- docs/plantuml/conv_from_puml_into_svg.sh
+++ docs/plantuml/conv_from_puml_into_svg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # なぜこんなことをしているのか：Dockerの`plantuml/plantuml`イメージは日本語フォントを持っていないため、SVGを生成すると描画が崩れる。そのため、`plantuml/plantuml-server`イメージを使ってローカルでPlantUMLサーバーを立ち上げ、そこに`*.puml`ファイルをPOSTして`*.svg`を出力する。
-docker run --rm -u $(id -u):$(id -g) -it -v .:/data --entrypoint /bin/sh plantuml/plantuml-server -c '
+docker run --rm -u $(id -u):$(id -g) -v .:/data --entrypoint /bin/sh plantuml/plantuml-server -c '
 /entrypoint.sh  > /dev/null 2>&1 &
 until curl -s -f http://localhost:8080/svg > /dev/null; do
   sleep 1
