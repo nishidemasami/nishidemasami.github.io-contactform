54a44068d046aea1a9459d272d3e7ee6de556822
NISHIDE, Masami <nishidemasammi@gmail.com>
2026-05-13 13:45:02 +0900

    Add SVG generation script and update CI/CD workflow for PlantUML files

---
+++ docs/plantuml/conv_from_puml_into_svg.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# なぜこんなことをしているのか：Dockerの`plantuml/plantuml`イメージは日本語フォントを持っていないため、SVGを生成すると描画が崩れる。そのため、`plantuml/plantuml-server`イメージを使ってローカルでPlantUMLサーバーを立ち上げ、そこに`*.puml`ファイルをPOSTして`*.svg`を出力する。
+docker run --rm -u $(id -u):$(id -g) -it -v .:/data --entrypoint /bin/sh plantuml/plantuml-server -c '
+/entrypoint.sh  > /dev/null 2>&1 &
+until curl -s -f http://localhost:8080/svg > /dev/null; do
+  sleep 1
+done
+find /data -name "*.puml" | while read -r puml; do
+  svg="${puml%.puml}.svg"
+  if [ ! -f "$svg" ]; then
+    echo "Generating $svg from $puml"
+    curl -H "Content-Type: text/plain; charset=UTF-8" -s -f --data-binary @"$puml" http://localhost:8080/svg > "$svg"
+  fi
+done
+exit
+'
